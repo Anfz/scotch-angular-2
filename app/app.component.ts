1e68d22cabeea5628e04642111ff545c82d039e9
Chris Beaver <chrisb@pinpointcomms.com.au>
2016-12-14 06:46:08 +1000

    basic html

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app', 
   template: `
+
+    <header> 
+      <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">My Angular 2 App!</a>
+        </div>
+      </nav>
+    </header>
     <div class="jumbotron">
       <h1>Welcome to our App! :D</h1>
     </div>
+
+    <footer class="text-center">
+      Chris Copied this
+    </footer>
     `, 
     styles: [`
     .jumbotron {box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
